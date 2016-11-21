c3acbb5e0de69504e65dc51109cd3861fbe0d895
Remy Willems <remy.willems@ortec-finance.com>
2016-11-21 18:47:43 +0100

    Added some more polymorphic testcases.

--- src/test/scala/PolymorphicTypes.scala
+++ src/test/scala/PolymorphicTypes.scala
@@ -10,6 +10,20 @@ class PolymorphicTypes extends FunSuite {
     assert(!StaticChecker.check(program))
   }
 
+  test("identitySquareIsNoInt") {
+    val identity = new Lambda("x", new Variable("x"))
+    val identity2 = new Lambda("x", new Variable("x"))
+    val program = Application(identity, identity2)
+    assert(!StaticChecker.check(program))
+  }
+
+  test("identitySquareIsNoInt2") {
+    val identity = new Lambda("x", new Variable("x"))
+    val identity2 = new Lambda("x", new Variable("x"))
+    val program = Add(Application(identity, identity2), Const(2))
+    assert(!StaticChecker.check(program))
+  }
+
   test("lambdaApplication") {
     val program = Application(new Lambda("x", new Variable("x")), Const(3))
     assert(StaticChecker.check(program))
@@ -24,6 +38,13 @@ class PolymorphicTypes extends FunSuite {
   test("reuseIdentity2") {
     val identity = new Lambda("x", new Variable("x"))
     val program = new Let("identity", identity, Application(new Variable("identity"), Application(new Variable("identity"), Const(3))))
+    assert(StaticChecker.check(program))
+  }
+
+  test("const" ) {
+    val const = new Lambda("x" , new Lambda("y", new Variable("x")))
+    val program = new Let("const", const, new Let("constSquare", Application(new Variable("const"), new Variable("const")),
+      Application(new Variable("constSquare"), Application(new Variable("const"), Const(3)))))
     assert(StaticChecker.check(program))
   }
 }
