153b1ae822ad61514c96c9aa471241354c1072c6
Remy Willems <remy.willems@ortec-finance.com>
2016-11-21 18:49:24 +0100

    Added TODO.

--- src/main/scala/constraints/types/Specialization.scala
+++ src/main/scala/constraints/types/Specialization.scala
@@ -17,7 +17,7 @@ case class Specialization(var specialized: Type, var template: Type) extends Typ
   override def apply(solver: ConstraintSolver): Boolean = {
     val constraintTypes = solver.constraints.diff(Seq(this)).flatMap(c => c.boundTypes) //TODO kan deze diff niet weg?
     val constraintVariables: Set[TypeVariable] = constraintTypes.flatMap(t => t.variables).toSet
-    if (constraintVariables.intersect(template.variables).isEmpty)
+    if (constraintVariables.intersect(template.variables).isEmpty) //TODO misschien kan dit bepalen of een type unconstrained is wel al wanneer het in de environment gestopt wordt. dan hoeft het maar een keer.
     {
       val instantiatedTemplate = template.specialize(solver.factory)
       if (!solver.unifyTypes(specialized, instantiatedTemplate))
