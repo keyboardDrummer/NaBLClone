daca990350ddf364e615f4d51305f0de04cd68f8
Remy Willems <remy.willems@ortec-finance.com>
2016-11-21 18:44:00 +0100

    Fix.

--- src/main/scala/language/types/IntLanguageType.scala
+++ src/main/scala/language/types/IntLanguageType.scala
@@ -4,7 +4,6 @@ import constraints.scopes.objects.Scope
 import constraints.types.TypesAreEqual
 import constraints.types.objects.Type
 import constraints.{Constraint, ConstraintBuilder}
-import IntType
 
 object IntLanguageType extends LanguageType {
   override def constraints(builder: ConstraintBuilder, _type: Type, scope: Scope): Seq[Constraint] = Seq(TypesAreEqual(_type, IntType))
