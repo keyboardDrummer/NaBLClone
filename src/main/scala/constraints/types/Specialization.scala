20460c62c726fc5efbea2c3264795b52d7a74c0e
Remy Willems <rgv.willems@gmail.com>
2016-11-22 19:50:55 +0100

    Fix.

--- src/main/scala/constraints/types/Specialization.scala
+++ src/main/scala/constraints/types/Specialization.scala
@@ -5,9 +5,11 @@ import constraints.ConstraintSolver
 
 case class Specialization(var specialized: Type, var template: Type) extends TypeConstraint
 {
+  var instantiatedTemplate: Option[Type] = None //this is a pretty weird hack to make sure that when instantiating types in unifyTypes, all types are correctly updated.
   override def instantiateType(variable: TypeVariable, instance: Type): Unit = {
     specialized = specialized.instantiateType(variable, instance)
     template = template.instantiateType(variable, instance)
+    instantiatedTemplate = instantiatedTemplate.map(t => t.instantiateType(variable, instance))
   }
 
   override def boundTypes: Set[Type] = Set(specialized)
@@ -18,8 +20,8 @@ case class Specialization(var specialized: Type, var template: Type) extends Typ
     if (constraintVariables.intersect(template.variables).isEmpty) //TODO misschien kan dit bepalen of een type unconstrained is wel al wanneer het in de environment gestopt wordt. dan hoeft het maar een keer.
     {
       val mapping = template.variables.map(v => (v,solver.factory.typeVariable)).toMap
-      val instantiatedTemplate = template.specialize(mapping)
-      if (!solver.unifyTypes(specialized, instantiatedTemplate))
+      instantiatedTemplate = Some(instantiatedTemplate.getOrElse(template.specialize(mapping)))
+      if (!solver.unifyTypes(specialized, instantiatedTemplate.get))
         return false
     }
     else
