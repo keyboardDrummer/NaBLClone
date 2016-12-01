ee561b94c16c6e94bc9378d59a280b7ec2c5d660
Remy Willems <remy.willems@ortec-finance.com>
2016-12-01 12:40:27 +0100

    Fixed bug.

--- src/main/scala/constraints/types/objects/TypeApplication.scala
+++ src/main/scala/constraints/types/objects/TypeApplication.scala
@@ -24,4 +24,19 @@ case class TypeApplication(override val function: Type, var arguments: Seq[Type]
   override def fullyApplied: Boolean = arguments.forall(a => a.fullyApplied)
 
   override def toString: String = function + (if (arguments.nonEmpty) "<" + arguments.map(a => a.toString).reduce((a, b) => a + ", " + b) + ">" else "")
+
+  def canEqual(other: Any): Boolean = other.isInstanceOf[TypeApplication]
+
+  override def equals(other: Any): Boolean = other match {
+    case that: TypeApplication =>
+      (that canEqual this) &&
+        function == that.function &&
+        arguments == that.arguments
+    case _ => false
+  }
+
+  override def hashCode(): Int = {
+    val state = Seq(function, arguments)
+    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+  }
 }
