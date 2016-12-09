9743c38659d1351257c522525c67c04d550ac9d5
Remy Willems <remy.willems@ortec-finance.com>
2016-12-09 19:20:21 +0100

    Deleted algebraic datatypes

--- src/test/scala/UnionTypes.scala
+++ src/test/scala/UnionTypes.scala
@@ -3,29 +3,10 @@ import language.expressions.Add
 import language.modules.{Binding, Module}
 import language.structs._
 import language.types.{BoolType, IntType}
-import language.unionTypes.{Case, Match, UnionOption, UnionType}
 import modes.MachineChecker
 import org.scalatest.FunSuite
 
 class UnionTypes extends FunSuite with LanguageWriter {
-
-  test("basic") {
-    val usage = Binding("main", Match("i" $ 3, Seq(Case("b","x",1), Case("i", "x", Add(2,"x")))))
-    val module = Module("module", Seq(usage), Seq(UnionType("intOrBool", Seq(UnionOption("i", IntType), UnionOption("b", BoolType)))))
-    Checker.check(Program(Seq(module)), skip = Set(MachineChecker))
-  }
-
-  test("basicFailBoolConstructorDoesNotContainInt") {
-    val usage = Binding("main", Match("i" $ 3, Seq(Case("b","x","x"), Case("i", "x", Add(2,"x")))))
-    val module = Module("module", Seq(usage), Seq(UnionType("intOrBool", Seq(UnionOption("i", IntType), UnionOption("b", BoolType)))))
-    Checker.fail(Program(Seq(module)))
-  }
-
-  test("basicFailPassBooleanToIntegerConstructor") {
-    val usage = Binding("main", Match("i" $ true, Seq(Case("b","x",1), Case("i", "x", Add(2,"x")))))
-    val module = Module("module", Seq(usage), Seq(UnionType("intOrBool", Seq(UnionOption("i", IntType), UnionOption("b", BoolType)))))
-    Checker.fail(Program(Seq(module)))
-  }
 
   test("matchTypeZero") {
     val baseType = Struct("intOrBool", Seq.empty)
